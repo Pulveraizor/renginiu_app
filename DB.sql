c362d1fb898920818e6b7ebe79533039d439d4b1
Benas Juskus <benas.juskus@gmail.com>
2024-06-19 15:12:05 +0300

    Added dummy data, added DB diagram

--- DB.sql
+++ DB.sql
@@ -9,7 +9,7 @@ CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE
 );
-
+INSERT INTO roles (name) VALUES ('admin'), ('user');
 -- Create the permissions table
 CREATE TABLE permissions (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -35,6 +35,7 @@ CREATE TABLE users (
     FOREIGN KEY (role_id) REFERENCES roles(id)
 );
 
+
 -- Create the event_categories table
 CREATE TABLE event_categories (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -52,6 +53,11 @@ INSERT INTO event_categories (name) VALUES
 ('Charity Event'),
 ('Sports Event');
 
+INSERT INTO users (email, password, role_id) VALUES 
+('admin@example.com', '$2b$10$hYsuYYFswAZzxi2FW/b2KuINCNVnJJ4bBtJIZXpKn4UT74w.ElFaK', 1),
+('user1@example.com', '$2b$10$hYsuYYFswAZzxi2FW/b2KuINCNVnJJ4bBtJIZXpKn4UT74w.ElFaK', 2),
+('user2@example.com', '$2b$10$hYsuYYFswAZzxi2FW/b2KuINCNVnJJ4bBtJIZXpKn4UT74w.ElFaK', 2);
+
 -- Create the events table
 CREATE TABLE events (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -64,6 +70,14 @@ CREATE TABLE events (
     FOREIGN KEY (created_by) REFERENCES users(id)
 );
 
+INSERT INTO events (name, description, category_id, date, created_by) VALUES
+('Rock Concert', 'A rock concert featuring local bands.', 1, '2023-06-01', 1),
+('Tech Expo', 'An exhibition showcasing the latest in technology.', 2, '2023-07-15', 1),
+('Modern Art Show', 'A display of modern art from local artists.', 3, '2023-08-20', 2),
+('Gourmet Food Fair', 'A fair featuring gourmet food from around the world.', 4, '2023-09-10', 2),
+('Fashion Week', 'A week-long event showcasing the latest fashion trends.', 5, '2023-10-05', 3),
+('Startup Networking', 'A networking event for startups and investors.', 6, '2023-11-01', 3);
+
 CREATE TABLE ratings (
     id INT AUTO_INCREMENT PRIMARY KEY,
     event_id INT,
@@ -73,8 +87,19 @@ CREATE TABLE ratings (
     FOREIGN KEY (event_id) REFERENCES events(id)
 );
 
+INSERT INTO ratings (event_id, rated_by, rating) VALUES
+(1, 2, 5),
+(1, 3, 4),
+(2, 2, 5),
+(3, 1, 4),
+(4, 1, 3),
+(5, 2, 4),
+(5, 3, 5),
+(6, 2, 4),
+(6, 3, 5);
+
 -- Insert initial roles
-INSERT INTO roles (name) VALUES ('admin'), ('user');
+
 
 -- Insert initial permissions
 INSERT INTO permissions (name) VALUES ('create_event'), ('edit_event'), ('delete_event'), ('view_event'), ('delete_user');
@@ -87,3 +112,4 @@ INSERT INTO has_permissions (role_id, permission_id) VALUES
 (1, 4), -- admin can view_event
 (2, 4), -- user can view_event
 (2,1); -- user can create_event
+
